06b023788f4f343d4d6647d43442193db143d0ce
christmknd <christmokonda@yahoo.fr>
2020-08-19 15:32:51 +0200

    initialisation des composants Home,Header et Footer

---
+++ vuejs-central/src/components/Header.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<template>
+    <div class="nav">
+        
+    </div>
+</template>
---
+++ vuejs-central/src/components/Home.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<template>
+    <div class="homepage">
+        <h1>Vue Central</h1>
+        <p>Bienvenue sur <strong>Vue Central</strong>. <br>
+         Simple, minimaliste,efficace</p>
+    </div>
+</template>
